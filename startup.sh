0e93c2966b9639a67dcdb34489b8306bf2b9ac80
Bruce Modell <73072824+brucmod@users.noreply.github.com>
2020-12-14 14:09:42 -0600

    startup shell

---
+++ startup.sh
@@ -0,0 +1,3 @@
+terraform apply -input=false -auto-approve
+for x in `seq 0 4`; do ssh-keygen -R 10.224.114.10$x; ./sshcopy.sh pureuser@10.224.114.10$x; done
+ansible-playbook -i inventory.ini prereqs.yaml --user pureuser --extra-vars "ansible_sudo_pass=pureuser"
