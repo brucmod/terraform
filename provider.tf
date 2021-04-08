18dee9fb0df4b530c0e5cc9bfa3cdfbe2b6104e3
Bruce Modell <73072824+brucmod@users.noreply.github.com>
2021-04-08 11:15:41 -0500

    Changed to CDW

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 
 provider "vsphere" {
-  user           = "administrator@vsphere.local"
-  password       = "Pure123$$!"
-  vsphere_server = "10.224.112.45"
+  user           = "brucem"
+  password       = "Madne$$@2020"
+  vsphere_server = "10.89.8.130"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
