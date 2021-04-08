3d6fa505a0cd02e1f9c63e916f4f31cb1f4a6ac8
Bruce Modell <73072824+brucmod@users.noreply.github.com>
2021-04-08 11:16:57 -0500

    changed to CDW

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
-for x in `seq 0 4`; do ssh-keygen -R 10.224.114.10$x; ./sshcopy.sh pureuser@10.224.114.10$x; done
+for x in `seq 0 3`; do ssh-keygen -R 10.89.13.48+$x; ./sshcopy.sh pureuser@10.89.13.48+$x; done
 ansible-playbook -i inventory.ini prereqs.yaml --user pureuser --extra-vars "ansible_sudo_pass=pureuser"
